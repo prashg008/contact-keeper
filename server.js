2a8b66425360cc4ba76298b81a98ca688860a870
prashanth <prashanthg008@gmail.com>
2020-03-23 19:04:55 +0530

    backend api's

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ msg: "welcome to Contact Keeper Api" }));
+// Connect Database
+connectDB();
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
-app.use("/api/users", require("./routes/users"));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
